5d5dd5e2ea27fd17dcf0523ac16136d66054a935
Dennis Xiao <dennisboys@gmail.com>
2016-12-19 18:19:28 +1100

    added animated css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
+    // animate the items
     $(".item")
-        .mouseover(function() {
+        .mouseover(function() {            
             $(this).css('opacity', '0.6');
+            $(this).find(".caption").addClass('animated wobble');
         })
         .mouseout(function() {
             $(this).css('opacity', '1');
+            $(this).find(".caption").removeClass('animated wobble');            
         });    
+
+    // seed data
+    const nameArr = [
+        "Dennis", "Zoe", "Jasen",
+        "Tom", "John", "Ricky",
+        "Ken", "Bob", "Nancy"
+    ]
 
 });
